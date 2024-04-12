af94ef9e7f5c2a4be25ddf02804780ae2084dd6b
Evgeny Kulinichev <ekulinichev@taker.io>
2024-04-12 16:34:43 +0300

    reset .gitignore

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "primereact/resources/themes/lara-light-cyan/theme.css";
-import App from "@/app/App";
 import reportWebVitals from "./app/reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import App from "@/app/App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
