bd351efdbb1daa1b9a0d03fa802f3af739ff07d7
Evgeny Kulinichev <gekin010@ya.ru>
2024-04-14 12:48:49 +0300

    Users table

--- frontend/src/pages/Users/Users.tsx
+++ frontend/src/pages/Users/Users.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import s from "./Users.module.scss";
 import axios from "axios";
 import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import User from "@/shared/interfaces/model/user.interface";
@@ -14,36 +14,10 @@ axios.defaults.withCredentials = true;
 export default function () {
   const { users, count, isLoading } = useUsers();
 
-  const options = {
-    page: 1,
-    perPage: 10,
-  };
-  const [search, setSearch] = useState("");
-  const [page, setPage] = useState(options.page);
-
-  const searchInputOnChange = (e: ChangeEvent) => {
-    // @ts-ignore
-    const { value } = e.target;
-    setSearch(value);
-  };
-
-  const onSubmitHandler = async (e: FormEvent) => {
-    e.preventDefault();
-
-    setPage(1);
-  };
-
   return (
     <Body>
-      <form onSubmit={onSubmitHandler}>
-        <input
-          name="user-search"
-          value={search}
-          placeholder="Поиск пользователей: Имя, телефон или email"
-          onChange={searchInputOnChange}
-        />
-      </form>
-      <div className="block-table">
+      <h1>Пользователи</h1>
+      <div className={s.Table}>
         <table>
           <thead>
             <tr>
@@ -62,13 +36,6 @@ export default function () {
             ))}
           </tbody>
         </table>
-
-        <Pagination
-          count={count}
-          perPage={options.perPage}
-          currentPage={page}
-          nextPage={(page: number) => setPage(page)}
-        />
       </div>
     </Body>
   );
